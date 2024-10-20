1c3383cdc99bb4ac97928efab2227fe5ef0c8659
joaolfp <joaolucasfp2001@gmail.com>
2024-10-20 17:08:02 -0300

    Fix indent code in field_results

--- src/fields_results.rs
+++ src/fields_results.rs
@@ -1,33 +1,26 @@
-use demand::{
-    DemandOption, 
-    Input, 
-    MultiSelect, 
-    Spinner, 
-    SpinnerStyle
-};
+use demand::{DemandOption, Input, MultiSelect, Spinner, SpinnerStyle};
 
-use std::{thread::sleep, time::Duration};
 use std::process::Command;
+use std::{thread::sleep, time::Duration};
 
 use crate::spm::*;
 
 pub struct FieldsResults;
 
 impl FieldsResults {
-
 	pub fn result() {
-        let project_name = Self::project_name_input();
+		let project_name = Self::project_name_input();
 		let selected = Self::multi_select_input();
 
-        Self::loading();
+		Self::loading();
 
 		Spm::create_spm(&project_name, selected);
 
-        Self::command_open_xcode(project_name);
+		Self::command_open_xcode(project_name);
 	}
 
-    fn project_name_input() -> String {
-        let notempty_minlen = |s: &str| {
+	fn project_name_input() -> String {
+		let notempty_minlen = |s: &str| {
 			if s.is_empty() {
 				return Err("Library name cannot be empty");
 			}
@@ -40,11 +33,11 @@ impl FieldsResults {
 			.validation(notempty_minlen);
 
 		let project_name = input.run().expect("error running input");
-        return project_name;
-    }
+		return project_name;
+	}
 
-    fn multi_select_input() -> Vec<&'static str> {
-        let multi_select = MultiSelect::new("Toppings")
+	fn multi_select_input() -> Vec<&'static str> {
+		let multi_select = MultiSelect::new("Toppings")
 			.description("Select your files")
 			.filterable(true)
 			.option(DemandOption::new("Changelog"))
@@ -58,25 +51,24 @@ impl FieldsResults {
 			.copied()
 			.collect();
 
-        return selected;
-    }
+		return selected;
+	}
 
-    fn loading() {
-        Spinner::new("\nBuilding the Package...")
-        .style(&SpinnerStyle::line())
-        .run(|_| {
-            sleep(Duration::from_secs(3));
-        })
-        .expect("error running spinner");
-    }
+	fn loading() {
+		Spinner::new("\nBuilding the Package...")
+			.style(&SpinnerStyle::line())
+			.run(|_| {
+				sleep(Duration::from_secs(3));
+			})
+			.expect("error running spinner");
+	}
 
-    fn command_open_xcode(project_name: String) {
-        let command = format!("cd {} && open Package.swift", project_name);
-        Command::new("sh")
-            .arg("-c")
-            .arg(command)
-            .spawn()
-            .expect("Failed to open Xcode");
-    }
-
+	fn command_open_xcode(project_name: String) {
+		let command = format!("cd {} && open Package.swift", project_name);
+		Command::new("sh")
+			.arg("-c")
+			.arg(command)
+			.spawn()
+			.expect("Failed to open Xcode");
+	}
 }
