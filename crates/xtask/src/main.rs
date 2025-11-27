ca484c0fb6e5517bdfa55498724515fd6b67f92f
joaolfp <joaolucasfp2001@gmail.com>
2025-11-27 00:21:35 -0300

    feat: setup xtask crates

--- crates/xtask/src/main.rs
+++ crates/xtask/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-	println!("Hello, world!");
+use xshell::{cmd, Shell};
+use demand::Input;
+
+fn main() -> anyhow::Result<()> {
+	header();
+	let shell = Shell::new()?;
+	input_option_validation(shell)?;
+
+	Ok(())
+}
+
+fn header() {
+	println!("1️⃣  Run spm-swift-package");
+	println!("2️⃣  Run all testes");
+	println!("3️⃣  Publish package");
+	println!("4️⃣  Running cargo publish (dry-run)");
+	println!();
+}
+
+fn input_option_validation(shell: Shell) -> anyhow::Result<()> {
+	let validation_input = |s: &str| {
+        if s.is_empty() {
+            return Err("Input cannot be empty");
+        }
+
+        Ok(())
+    };
+
+    let option_input = Input::new("Choose an option: ")
+        .prompt("Option: ")
+        .validation(validation_input);
+
+    let option = option_input.run().expect("error running input");
+
+	match option.as_str() {
+		"1" => {
+			println!("🚀 Running spm-swift-package");
+			println!();
+			cmd!(shell, "cargo run").run()?;
+		}
+		"2" => {
+			println!("✅ ❌ Running all testes");
+			println!();
+			cmd!(shell, "mise test").run()?;
+		}
+		"3" => {
+			println!("📦 Publishing package");
+			println!();
+			cmd!(shell, "cargo publish").run()?;
+		}
+		"4" => {
+			println!("📦 Running cargo publish (dry-run)...");
+			println!();
+			cmd!(shell, "cargo publish --dry-run").run()?;
+		}
+		_ => {
+			println!("Invalid option");
+		}
+	}
+
+	Ok(())
 }
