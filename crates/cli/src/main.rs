1b1c73a46a5f8f73c25b9ec91d87815815339ace
joaolfp <joaolucasfp2001@gmail.com>
2025-12-02 14:33:31 -0300

    feat: support GUI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,13 +1,32 @@
 mod domain;
 mod presentation;
+mod ui;
 
+use clap::{Parser, Subcommand};
 use presentation::cli_controller::CliController;
 use presentation::header::Header;
 
+#[derive(Parser)]
+struct Args {
+	#[command(subcommand)]
+	command: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+	UI,
+}
+
 #[tokio::main]
 async fn main() {
-	let header = Header::show();
-	println!("{header}");
+	let args = Args::parse();
 
-	let _ = CliController::execute_flow().await;
+	if let Some(Command::UI) = args.command {
+		let _ = ui::spm_view::run();
+	} else {
+		let header = Header::show();
+		println!("{header}");
+
+		let _ = CliController::execute_flow().await;
+	}
 }
